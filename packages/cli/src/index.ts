3818b2a66a204df27d6522cf960e79a88f810356
joungminsung <joungminsung080830@gmail.com>
2026-04-20 14:04:44 +0900

    Revert "feat(cli): add model management command and improve init UX"
    This reverts commit 946045bda3bd39f2b109d39b7a8afc8e326b6382.

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -22,7 +22,6 @@ import { searchCommand } from './commands/search.js'
 import { completionCommand } from './commands/completion.js'
 import { upgradeCommand } from './commands/upgrade.js'
 import { backupCommand, restoreCommand } from './commands/backup.js'
-import { modelCommand } from './commands/model.js'
 
 const program = new Command()
 program
@@ -49,6 +48,5 @@ program.addCommand(completionCommand())
 program.addCommand(upgradeCommand())
 program.addCommand(backupCommand())
 program.addCommand(restoreCommand())
-program.addCommand(modelCommand())
 
 program.parse()
